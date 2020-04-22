9516c5ccf036177fcb6f4cbf9473c781c371367c
Cociobanu Victor-Andrei <62379953+Vikcoc@users.noreply.github.com>
2020-04-22 15:46:05 +0300

    Add files via upload

--- Tema2/src/PesteLimite.cpp
+++ Tema2/src/PesteLimite.cpp
@@ -20,6 +20,48 @@ PesteLimite::PesteLimite(const int & acc, const int & lim) noexcept : vreau(acc)
         fu[aux.size()]=0;
     }
 }
+PesteLimite::PesteLimite(PesteLimite const & x)noexcept : vreau(x.vreau), am(x.am)
+{
+    if(vreau<0)
+    {
+        std::string aux={"S-a incercat accesarea pozitiei negative "+(std::to_string(vreau))+" si nu merge"};
+        fu = new char[aux.size()];
+        for(unsigned i = 0; i<aux.size() ; ++i)
+            fu[i]=aux[i];
+        fu[aux.size()]=0;
+
+    }
+    else
+    {
+        std::string aux = {"S-a incercat accesarea pozitiei "+(std::to_string(vreau))+" in afara limitei de "+(std::to_string(am))};
+        fu = new char[aux.size()];
+        for(unsigned i = 0; i<aux.size() ; ++i)
+            fu[i]=aux[i];
+        fu[aux.size()]=0;
+    }
+}
+PesteLimite & PesteLimite::operator= (PesteLimite const & x)noexcept
+{
+    vreau=x.vreau;
+    am=x.am;
+    if(vreau<0)
+    {
+        std::string aux={"S-a incercat accesarea pozitiei negative "+(std::to_string(vreau))+" si nu merge"};
+        fu = new char[aux.size()];
+        for(unsigned i = 0; i<aux.size() ; ++i)
+            fu[i]=aux[i];
+        fu[aux.size()]=0;
+
+    }
+    else
+    {
+        std::string aux = {"S-a incercat accesarea pozitiei "+(std::to_string(vreau))+" in afara limitei de "+(std::to_string(am))};
+        fu = new char[aux.size()];
+        for(unsigned i = 0; i<aux.size() ; ++i)
+            fu[i]=aux[i];
+        fu[aux.size()]=0;
+    }
+}
 
 PesteLimite::~PesteLimite()
 { delete[] fu;}
